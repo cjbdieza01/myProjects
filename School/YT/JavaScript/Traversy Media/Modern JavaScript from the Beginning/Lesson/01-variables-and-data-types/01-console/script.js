266da994fa1ad2d7bedeb9db4e94ef23c5130ef0
Cris Jordan Dieza <cjbdieza@gmail.com>
2023-09-25 22:06:52 +0800

    add code

---
+++ School/YT/JavaScript/Traversy Media/Modern JavaScript from the Beginning/Lesson/01-variables-and-data-types/01-console/script.js
@@ -0,0 +1,9 @@
+console.log('hello')
+console.error('Error');
+console.warn('Warning');
+console.table({name: 'John', age: 21, email: 'john@email.com'});
+console.group('simple');
+console.log('Hello');
+console.error('Error');
+console.warn('Warning');
+console.groupEnd();
