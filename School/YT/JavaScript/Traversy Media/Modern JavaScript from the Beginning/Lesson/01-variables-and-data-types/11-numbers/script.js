809771545710518cc49e677957741d97a8b68e25
Cris Jordan Dieza <cjbdieza@gmail.com>
2023-09-30 14:25:12 +0800

    add code

---
+++ School/YT/JavaScript/Traversy Media/Modern JavaScript from the Beginning/Lesson/01-variables-and-data-types/11-numbers/script.js
@@ -0,0 +1,11 @@
+let x;
+const num = 5;
+x = num.toString();
+x = num.toString().length;
+x = num.toFixed(1);
+x = num.toPrecision(2);
+x = num.toExponential(1);
+// x = num.toLocaleString('en-US');
+// x = num.valueOf();
+
+console.log(typeof x, typeof num);
---
+++ School/YT/JavaScript/Traversy Media/Modern JavaScript from the Beginning/Lesson/01-variables-and-data-types/14-dates-and-time/script.js
@@ -0,0 +1,7 @@
+let d = new Date();
+d = d.toString();
+d = new Date(2021, 11, 10, 13, 20, 11);
+d = new Date('2021-12-12T12:30:12');
+d = new Date('07/10/2023 12:30:22');
+
+console.log(d);  
