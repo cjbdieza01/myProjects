ddc94f77f056e9e44931d9b20e54aada4d5c62c6
Cris Jordan Dieza <cjbdieza@gmail.com>
2023-11-02 20:04:01 +0800

    add lesson

---
+++ Books/Eloquent Javascript/Eloquent-javascript/script.js
@@ -0,0 +1,46 @@
+// function factorial(n) {
+//     if ( n == 0) {
+//         return 1;
+//     } else {
+//         return factorial(n - 1) * n;
+//     }
+// }
+
+// let num = 5
+// let total = factorial(num);
+// console.log(total);
+
+// console.log(true ? 2 : 1 );
+// console.log(false ? 2 : 1 );
+
+
+
+
+
+// let number = prompt(`Enter a number: `)
+// function factorial(n) {
+//     if (n == 0) {
+//         return 1;
+//     } else {
+//         return factorial(n - 1) * n
+//     }
+// }
+// let total = factorial(number);
+// alert(total); 5
+
+// let total = 0;
+// let count = 1;
+// while (count <= 10) {
+//     total += count;
+//     count++
+// }
+
+// x = 
+// console.log(sum(range(1, 4)));
+// console.log(total);
+
+// let X = 40;
+// let y = X;
+
+// X = 20;
+// console.log(X , y);
