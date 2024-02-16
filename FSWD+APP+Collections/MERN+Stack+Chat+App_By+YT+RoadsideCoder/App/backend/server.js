1b34ac02cf2e08e2c68ff0352de5909a56413f9f
Cris Jordan Dieza <cjbdieza@gmail.com>
2024-02-16 08:54:46 +0800

    add

--- FSWD+APP+Collections/MERN+Stack+Chat+App_By+YT+RoadsideCoder/App/backend/server.js
+++ FSWD+APP+Collections/MERN+Stack+Chat+App_By+YT+RoadsideCoder/App/backend/server.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const { chats } = require("./data/data");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send("API is Running");
+    res.send("API is Running ok");
 })
 
 app.get("/api/users", (req, res) => {
     res.send(chats)
 })
 
-app.listen(5500, console.log("Server Started on PORT 5500"))
+app.get("/api/users/:id", (req, res) => {
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat)
+})
+
+const PORT = process.env.PORT || 5500;
+
+app.listen(5500, console.log(`"Server Started on PORT ${PORT} "`))
