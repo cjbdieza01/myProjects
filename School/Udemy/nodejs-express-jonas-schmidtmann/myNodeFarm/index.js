f6173f3f08fcb0f003b6303ad11b6c793b95d9dc
@cjbdieza01 <cjbdieza@gmail.com>
2024-04-01 15:16:43 +0800

    add code

--- School/Udemy/nodejs-express-jonas-schmidtmann/myNodeFarm/index.js
+++ School/Udemy/nodejs-express-jonas-schmidtmann/myNodeFarm/index.js
@@ -9,24 +9,46 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8')
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
-
-
-
+const replaceTemplate = require('./modules/replaceTemplate')
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
-    
-    if (pathName === "/hello") {
+    const { query, pathname } = url.parse(req.url, true); 
+    // console.log(url);
+    if (pathname === "/hello") {
         res.end("SERVER")
         console.log(tempCard)
     } 
     // Overview Page
-    else if (pathName === "/overview" || pathName === "/") {
+    else if (pathname === "/overview" || pathname === "/") {
         res.writeHead(200, {
             'Content-Type': 'text/html',
             'My-Header' : 'Hello Pre'
         })
-        res.end(tempOverview)
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join();
+        const output = tempOverview.replace('@PRODUCT_CARDS@', cardsHtml);
+        res.end(output);
+
+    } else if (pathname === '/product') {
+        res.writeHead(200, {
+            "Content-Type": 'text/html'
+        });
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        // console.log(url);
+        // console.log(req.url);
+        // console.log(query);
+        // console.log(pathname);
+        // console.log(query);
+        res.end(output);
     }
+    
+    else {
+    res.writeHead(404, {
+      'Content-Type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+        const HTMLtext = `<h1>Page not found!</h1>`
+    res.end(HTMLtext);
+  }
 
 })
 
---
+++ School/Udemy/nodejs-express-jonas-schmidtmann/scratch.js
@@ -0,0 +1,7 @@
+const myNumber = [2, 4, 6, 8, 10];
+const doubleNumber = myNumber.map(number => {
+    return number*2;
+})
+
+console.log(myNumber);
+console.log(doubleNumber);
