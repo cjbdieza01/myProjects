c111c37267238faebc8a2b4ec64d4372d4da6d59
Cris Jordan Dieza <cjbdieza@gmail.com>
2024-05-11 00:45:06 +0800

    add

--- Books/Eloquent Javascript/Eloquent-javascript/script.js
+++ Books/Eloquent Javascript/Eloquent-javascript/script.js
@@ -1,5 +1,12 @@
-const hummus = (factor) => {
-    const ingredient = (amount, unit, name => {
-        
-    })
+function number(n) {
+  let a = n;
+  return () => a;
 }
+
+let b = number(7);
+console.log(b());
+
+let c = number(1);
+let d = number(2);
+
+console.log(c(), d());
